d5d1e07737dadc9538d02d4ed8dd07926ff134a9
Corps <zsq01@live.com>
2018-07-26 02:52:03 +0800

    Page test

---
+++ src/Pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <Toolbar title="Index" :links="links"></Toolbar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Toolbar from '../Components/Toolbar.vue'
+
+export default Vue.extend({
+    data:()=>({
+        links:[{name:"Index",link:"/"}]
+    }),
+    components:{
+        Toolbar
+    }
+})
+</script>
